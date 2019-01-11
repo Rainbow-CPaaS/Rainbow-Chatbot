680510c7ded26cafffd72c7a52ad57f10077cecb
berder <vincent.berder@al-enterprise.com>
2019-01-11 10:46:20 +0100

    update to add new config option for the rainbow node sdk

--- samples/sample_message.js
+++ samples/sample_message.js
@@ -3,8 +3,16 @@ const ChatBot = require("../index");
 const NodeSDK = require("rainbow-node-sdk");
 
 // Load bot identity
-const bot = require("./bot.json");          // Load bot identity
+//const bot = require("./bot.json");          // Load bot identity
 const scenario = require("./sample_message.json");  // Load scenario
+
+const json = require('comment-json');
+const fs = require('fs');
+const botfile = fs.readFileSync("./samples/bot.json");
+let txt = botfile.toString();
+let bot = json.parse(txt);
+
+json.stringify(bot, null, 2);
 
 let chatbot = null;
 let nodeSDK = null;
